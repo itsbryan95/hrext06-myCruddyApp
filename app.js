e6b9c61ec04978b96a0e210089f12095c9b8b31f
bryan lee <bryanlee@bryans-Air.fios-router.home>
2018-12-21 01:18:38 -0800

    completed myCruddyApp

--- app.js
+++ app.js
@@ -3,24 +3,29 @@ $(document).ready(function(){
 
   $('.log-in').on('click', function(){
     $('.start-options').html('');
-  })
+  });
 
   $('.btn-sign-up').on('click', function(){
     //$('.start-options').html('');
     $('.display').text('')
     $('.start-options').css({"display": 'none'});
     $('.sign-up').css({"display": 'block'});
+    $('.log-in-email').val("");
+    $('.log-in-password').val("");
   });
 
   $('.btn-create-account').on('click', function(){
     if($('.first-name').val() === '' || $('.last-name').val() === '' || $('.email').val() === '' 
       || $('.password').val() === '' || $('.password-check').val() === ''){
-      $('.display').text('Missing required fields');
+      //$('.display').text('Missing required fields');
+      alert('Missing required fields');
     } else 
     if (localStorage[$('.email').val()] !== undefined){
-      $('.display').text('This email is already being used');
+      //$('.display').text('This email is already being used');
+      alert('This email is already being used');
     } else if ($('.password').val() !== $('.password-check').val()){
-      $('.display').text('Password does not match');
+      //$('.display').text('Password does not match');
+      alert('Password does not match');
     } else {
       $('.display').text('');
       var accountObj = {};
@@ -32,9 +37,12 @@ $(document).ready(function(){
 
       $('.sign-up').css({"display": 'none'});
       $('.account-created').css({"display": 'block'});
-      console.log(localStorage[$('.email').val()]);
+      $('.first-name').val("");
+      $('.last-name').val("");
+      $('.email').val("");
+      $('.password').val("");
+      $('.password-check').val("");
     }
-    
   });
 
   $('.back-home').on('click', function(){
@@ -44,14 +52,15 @@ $(document).ready(function(){
 
   $('.btn-log-in').on('click', function(){
     //$('.start-options').css({"display": 'none'});
-
     if(localStorage.getItem($('.log-in-email').val()) === null){
-      $('.display').text('Your email or password is incorrect. Please try again.');
+      //$('.display').text('Your email or password is incorrect. Please try again.');
+      alert('Your email or password is incorrect. Please try again.')
     } else {
       var userObj = JSON.parse(localStorage[$('.log-in-email').val()]);
       var password = userObj.password;
       if($('.log-in-password').val() !== password){
-        $('.display').text('Your email or password is incorrect. Please try again.');
+        //$('.display').text('Your email or password is incorrect. Please try again.');
+        alert('Your email or password is incorrect. Please try again.');
       } else {
         $('.display').text('');
         loggedInUserObj.email = localStorage[$('.log-in-email').val()];
@@ -59,11 +68,12 @@ $(document).ready(function(){
         $('.start-options').css({"display": 'none'});
         //$('.display').text('Logged in!');
         $('.my-account').css({"display": 'block'});
+        $('.log-in-email').val("");
+        $('.log-in-password').val("");
       }
     }
-
-
   });
+
   $('.btn-delete-account').on('click', function(){
     $('.my-account').css({"display": 'none'});
     $('.delete-account').css({"display": 'block'});
@@ -71,18 +81,21 @@ $(document).ready(function(){
 
   $('.final-delete').on('click', function(){
     if(localStorage.getItem($('.delete-email').val()) === null || localStorage.getItem($('.delete-email').val()) !== loggedInUserObj.email){
-      $('.display').text('Your email or password is incorrect. Please try again.')
+      //$('.display').text('Your email or password is incorrect. Please try again.')
+      alert('Your email or password is incorrect. Please try again.');
     } else {
       var userObj = JSON.parse(localStorage[$('.delete-email').val()]);
       var password = userObj.password;
       if($('.delete-password').val() !== password){
-        $('.display').text('Your email or password is incorrect. Please try again.')
+        //$('.display').text('Your email or password is incorrect. Please try again.')
+        alert('Your email or password is incorrect. Please try again.');
       } else {
         $('.display').text('');
         localStorage.removeItem($('.delete-email').val());
         $('.delete-account').css({"display": 'none'});
         $('.start-options').css({"display": 'block'});
-
+        $('.delete-email').val("");
+        $('.delete-password').val("");
       }
     }
   });
